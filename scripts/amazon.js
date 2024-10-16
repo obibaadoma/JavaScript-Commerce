ea00edf607ccc45b365def798f663a68147c7fc9
Kweku-Adoma <akrasikonadu@yahoo.com>
2024-10-16 02:10:22 +0000

    use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,34 +1,6 @@
-const product = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCent: 1090
-},
-{
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Composite Basketball',
-  rating: {
-    stars: 4,
-    count: 127
-  },
-  priceCent: 2990
-},
-{
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name:'Adult Plain Cotton T-shirt-2 pack',
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  priceCent: 799
-}];
-
 let productsHTML = '';
 
-product.forEach((product)=>{
+products.forEach((product)=>{
   productsHTML += `
     <div class="product-container">
             <div class="product-image-container">
