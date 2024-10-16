98b93ae872893c327c74195e0a039cf57d6cb5c1
Kweku-Adoma <akrasikonadu@yahoo.com>
2024-10-16 18:19:24 +0000

    Move addToCart into Cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,19 @@
 export const cart = [];
+
+export function addToCart(productId){
+  let matchingItem;  
+  cart.forEach((cartItem)=>{
+      if(productId===cartItem.productId){
+          matchingItem=cartItem;
+      }
+    });
+      if(matchingItem){
+        matchingItem.Quantity+=1;
+      }
+      else{
+        cart.push({
+          productId: productId,
+          Quantity: 1
+        });
+      }
+}
